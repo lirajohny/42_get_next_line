6feb9ac49b59d7b395cda61147638e75bea4bcb4
Johny Lira <johnylira.dev@gmail.com>
2023-11-12 23:22:07 -0300

    Working: Passed one test, in the other it has issues with leaks. Removed comments

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jlira <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 12:55:13 by jlira             #+#    #+#             */
-/*   Updated: 2023/11/12 23:13:39 by jlira            ###   ########.fr       */
+/*   Updated: 2023/11/12 23:20:42 by jlira            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,13 +50,7 @@ static char	*read_remain(t_list *node)
 	if (pos >= 0)
 	{
 		buff_remain = ft_substr(node->remain, 0, pos + 1);
-				//printf("\tADDRESS | \033[1;91m %p \033[0m | \033[1;32m buff_remain: \033[0m |\033[1;34m %s \033[0m \n", buff_remain, buff_remain);
-
 		node->remain = node->remain + (pos + 1);
-				//printf("\tADDRESS | \033[1;91m %p \033[0m | \033[1;32m  update > node->remain - saving: \033[0m |\033[1;34m %s \033[0m| \n", buff_remain, buff_remain);
-
-			//printf("\033[1;33m LEAVING \033[0m\n");
-
 		return (buff_remain);
 	}
 	return (NULL);
@@ -65,32 +59,21 @@ static char	*read_remain(t_list *node)
 char	*fetch_line(t_list	*node, int fd, int pos)
 {
 	char	*buff;
-		//printf("\033[1;33m FETCH LINE FUNCTION \033[0m\n");
-
 
 	buff = malloc(sizeof(char) * BUFFER_SIZE + 1);
 	if (!buff)
 		return (NULL);
-	//printf("\t\033[1;32m  > REMAIN : \033[0m |\033[1;34m %s \033[0m|\n", node->remain);
-
 	while (node->bytes_read >= 1)
 	{
 		node->bytes_read = read(fd, buff, BUFFER_SIZE);
 		buff[node->bytes_read] = '\0';
-				//printf("\tADDRESS | \033[1;91m %p \033[0m | \033[1;32m BUFFER NOW: \033[0m |\033[1;34m %s \033[0m| \n", buff, buff);
-
 		pos = find_line(buff);
 		if (pos >= 0)
 		{
-						//printf("\t\033[1;32m  > LINE : \033[0m |\033[1;34m %s \033[0m|\n", node->line);
 			node->line = ft_strjoin(node->line, ft_substr(buff, 0, pos + 1));
-					//printf("\tADDRESS | \033[1;91m %p \033[0m | \033[1;32m TEMP: \033[0m |\033[1;34m %s \033[0m \n", node->line, node->line);
-
 			break ;
 		}
 		node->line = ft_strjoin(node->line, buff);
-				//printf("\t\033[1;32m  > LINE : \033[0m |\033[1;34m %s \033[0m|\n", node->line);
-
 	}
 	if (pos != -1 && pos + 1 < node->bytes_read)
 		node->remain = ft_substr(buff, pos + 1, ft_strlen(buff));
@@ -104,24 +87,16 @@ static char	*read_file(int fd, t_list *node)
 
 	if (node->remain != NULL)
 	{
-		//printf("\tADDRESS | \033[1;91m %p \033[0m | \033[1;32m  JUNTANDO BEF> LINE RET: \033[0m |\033[1;34m %s \033[0m| \n", node->line, node->line);
-		//printf("\tADDRESS | \033[1;91m %p \033[0m | \033[1;32m  JUNTANDO BEF> REMAIN RET: \033[0m |\033[1;34m %s \033[0m| \n", node->remain, node->remain);
 		node->line = ft_strdup(node->remain);
-		//printf("\tADDRESS | \033[1;91m %p \033[0m | \033[1;32m  JUNTANDO AFF> LINE RET: \033[0m |\033[1;34m %s \033[0m| \n", node->line, node->line);
-		//printf("\tADDRESS | \033[1;91m %p \033[0m | \033[1;32m  JUNTANDO AFF> REMAIN RET: \033[0m |\033[1;34m %s \033[0m| \n", node->remain, node->remain);
 		//free(node->remain);
 		node->remain = ft_strdup("");
 	}
 	next_line = fetch_line(node, fd, 0);
-	//printf("\tADDRESS | \033[1;91m %p \033[0m | \033[1;32m  > LINE RET: \033[0m |\033[1;34m %s \033[0m| \n", node->line, node->line);
- 	//printf("\tADDRESS | \033[1;91m %p \033[0m | \033[1;32m  > REMAIN RET: \033[0m |\033[1;34m %s \033[0m| \n", node->remain, node->remain);
 	return (next_line);
 }
 
 char	*get_next_line(int fd)
 {
-		//printf("\033[1;33m GET_NEXT_LINE FUNCTION \033[0m\n");
-
 	char				*next_line;
 	static t_list		*node;
 	char				*remain;
@@ -134,35 +109,17 @@ char	*get_next_line(int fd)
 		node->remain = ft_strdup("");
 		node->bytes_read = 2;
 	}
-	//printf(" ----------------- previous call ---------------- i\n");
- 	//printf("\t\t ∑∑ byte_read\033[1;33m %i \033[0m ∑∑ \n", node->bytes_read);
- 	//printf("\t\033[1;32m  > REMAIN INICIO: \033[0m |\033[1;34m %s \033[0m|\n", node->remain);
- 	//printf("\t\033[1;32m  > LINE INICIO: \033[0m |\033[1;34m %s \033[0m|\n", node->line);
- 	//printf("\t ADDRESS | \033[1;91m %p \033[0m | \033[1;32m  > REMAIN INICIO: \033[0m |\033[1;34m %s \033[0m| \n", node->remain, node->remain);
- 	//printf("\t ADDRESS | \033[1;91m %p \033[0m | \033[1;32m  > LINE INICIO: \033[0m |\033[1;34m %s \033[0m|  \n", node->line, node->line);
- 	//printf(" ----------------- current call ---------------- i\n");
- 	//printf("\t\t\t ∑∑ CALL \033[1;33m %i \033[0m ∑∑ \n", node->call);
 	if (fd < 0 || BUFFER_SIZE <= 0 || read(fd, &next_line, 0) < 0)
 		return (NULL);
 	remain = read_remain(node);
 	if (remain != NULL)
-	{
-		//printf("\033[1;32m  Returning: \033[0m |\033[1;34m %s \033[0m|\n", next_line);
- 		//printf("\033[1;32m  HAS: \033[0m |\033[1;34m %s \033[0m|\n", node->line);
- 		//printf("\033[1;32m  saving: \033[0m |\033[1;34m %s \033[0m|\n", node->remain);
 		return (remain);
-	}
 	else
 	{
 		if (node->bytes_read == 0 && node->call != 1)
 			return (NULL);
 		next_line = read_file(fd, node);
 	}
-	//printf("\033[1;32m  Returning: \033[0m |\033[1;34m %s \033[0m|\n", next_line);
- 		//printf("\033[1;32m  HAS: \033[0m |\033[1;34m %s \033[0m|\n", node->line);
- 		//printf("\033[1;32m  saving: \033[0m |\033[1;34m %s \033[0m|\n", node->remain);
-			//printf("\033[1;33m LEAVING \033[0m\n");
-
 	node->call++;
 	return (next_line);
 }
