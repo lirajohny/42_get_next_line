e6c8fe5004ae70b0bd01f9f4ab8cc675c54428c7
lirajohny <johnylira.dev@gmail.com>
2023-12-04 17:05:28 -0300

    today afternoon

--- gnl/get_next_line.c
+++ gnl/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jlira <jlira@student.42.rj>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/16 19:58:37 by jlira             #+#    #+#             */
-/*   Updated: 2023/12/04 16:49:34 by jlira            ###   ########.fr       */
+/*   Updated: 2023/12/04 17:03:10 by jlira            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,9 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int find_line(char *str)
+int	find_line(char *str)
 {
-	int index;
+	int	index;
 
 	index = 0;
 	if (str[0] == '\n')
@@ -30,30 +30,24 @@ int find_line(char *str)
 	return (0);
 }
 
-char *ft_get_line(struct s_list **list, int i, int j, int len)
+char	*copy_line(struct s_list **list, int len)
 {
-	t_list *current;
-	char *result;
+	t_list	*current;
+	char	*result;
+	int		i;
+	int		j;
 
-	current = *list;
-	while (current->next != NULL)
-	{
-		len += ft_strlen(current->content);
-		current = current->next;
-	}
-	j = find_line(current->content);
-	if (j == 0)
-		len += ft_strlen(current->content);
-	len += j;
 	result = (char *)malloc(sizeof(char) * len + 1);
 	current = *list;
-	while (current != NULL) {
+	i = 0;
+	while (current != NULL)
+	{
 		j = 0;
 		while (current->content[j] != '\0' && i < len)
 		{
 			result[i++] = current->content[j++];
 			if (current->content[j - 1] == '\n')
-				break;
+				break ;
 		}
 		current = current->next;
 	}
@@ -61,19 +55,42 @@ char *ft_get_line(struct s_list **list, int i, int j, int len)
 	return (result);
 }
 
-int read_file(t_list **list, int fd)
+char	*fetch_line(struct s_list **list)
 {
-	char *buffer;
-	t_list *new;
+	t_list	*current;
+	int		i;
+	int		len;
+
+	current = *list;
+	len = 0;
+	i = 0;
+	while (current->next != NULL)
+	{
+		len += ft_strlen(current->content);
+		current = current->next;
+	}
+	i = find_line(current->content);
+	if (i == 0)
+		len += ft_strlen(current->content);
+	len += i;
+	return (copy_line(list, len));
+}
+
+int	read_file(t_list **list, int fd)
+{
+	char	*buffer;
+	t_list	*new;
 
 	new = *list;
 	if (new->content[0] == '\n' || find_line(new->content) > 0)
 		return (-1);
-	while (new->bytes_read > 0) {
+	while (new->bytes_read > 0)
+	{
 		buffer = (char *)malloc(sizeof(char) * (BUFFER_SIZE + 1));
 		new->bytes_read = read(fd, buffer, BUFFER_SIZE);
 		buffer[new->bytes_read] = '\0';
-		if (new->bytes_read > 0) {
+		if (new->bytes_read > 0)
+		{
 			if (find_line(buffer) > 0 || buffer[0] == '\n')
 			{
 				new->next = ft_lstnew(buffer, new->bytes_read, 0);
@@ -89,13 +106,13 @@ int read_file(t_list **list, int fd)
 	return (-2);
 }
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
-	t_list *head;
-	char *next_line;
-	static char *remain;
-	t_list *last;
-	static int check_error;
+	t_list		*head;
+	char		*next_line;
+	static char	*remain;
+	t_list		*last;
+	static int	check_error;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
@@ -111,7 +128,7 @@ char *get_next_line(int fd)
 	if (check_error == -1)
 		next_line = ft_substr(last->content, 0, find_line(last->content));
 	else
-		next_line = ft_get_line(&head, 0, 0, 0);
+		next_line = fetch_line(&head);
 	remain = ft_substr(last->content, find_line(last->content),
 			ft_strlen(last->content));
 	ft_free(&head);
